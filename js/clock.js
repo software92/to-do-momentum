12d43e60ab923be3679c7862c949fc739dd9141b
jsw <allmie@naver.com>
2022-01-27 13:53:01 +0900

    2.3 Clock.css, Global.css

--- js/clock.js
+++ js/clock.js
@@ -3,11 +3,12 @@ const timeline = document.querySelector("h2.clock");
 const clock = () => {
   const time = new Date();
 
-  const hours = time.getHours();
+  const hours = String(time.getHours()).padStart(2, "0");
   const minutes = String(time.getMinutes()).padStart(2, "0");
-  const seconds = String(time.getSeconds()).padStart(2, "0");
+  //   const seconds = String(time.getSeconds()).padStart(2, "0");
 
-  timeline.innerHTML = `${hours}:${minutes}:${seconds}`;
+  //   timeline.innerHTML = `${hours}:${minutes}:${seconds}`;
+  timeline.innerHTML = `${hours}:${minutes}`;
 };
 
 clock();
