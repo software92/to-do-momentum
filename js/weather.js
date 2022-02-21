43f8ef5725f513f3c93477b40c9f8b15aef430e1
jsw <allmie@naver.com>
2022-02-21 14:20:35 +0900

    2.7 Weather.js (Mixed content issue)

--- js/weather.js
+++ js/weather.js
@@ -5,14 +5,14 @@ const geoSuccess = (result) => {
 
   const lat = result.coords.latitude;
   const lon = result.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
 
-  fetch(
-    `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
-  )
+  fetch(url)
     .then((response) => response.json())
     .then((json) => {
       weather.innerHTML = `${json.name} ${json.main.temp} °C`;
     });
 };
+const geoError = () => console.log("Can`t find you.");
 
-navigator.geolocation.getCurrentPosition(geoSuccess);
+navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
